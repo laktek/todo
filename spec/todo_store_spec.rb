4de734400f356c58e54444ccafadcfc4d8f8f9fb
Manuel Stuefer <mstuefer@gmail.com>
2014-07-17 23:09:28 +0200

    Add todo_task_spec, Del obsolete list.rb and their spec todo_list_spec

--- spec/todo_store_spec.rb
+++ spec/todo_store_spec.rb
@@ -1,8 +1,18 @@
 require 'todo/store'
 
-describe "Todo::CLI" do
-  it "should read the YAML file and output Todo::List hash" do
-    #list = {"Buy milk" => ["important", "home"]}
-    #Todo::Store.read(list.to_yaml).should be_an_instance_of(Todo::List)
+describe Todo::Store, 'read' do
+
+  let(:filename) { ".todo/list.yml" }
+
+  # TODO
+  it "reads the todo/list.yml from the filesystem" do
+    # store = class_double('Todo::Store')
+    # expect(store).to receive(:read).with(filename)
+    # store.read(filename)
+
+    # file = class_double('File')
+    # expect(file).to receive(:open).with(filename)
+    # Todo::Store.read(filename)
   end
+
 end
